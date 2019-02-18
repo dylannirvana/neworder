8685d5aed18a97c5a0dcc4605d14d56ce66bc3f5
Dylan Nirvana <dylan@dylannirvana.com>
2019-02-18 08:45:25 -0500

    TODO: new_sku:old_gridorder

--- src/App.js
+++ src/App.js
@@ -37,8 +37,7 @@ class App extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.updateData = this.updateData.bind(this)
         this.renderData = this.renderData.bind(this)  
-        this.handleClick = this.handleClick.bind(this)
-        
+        this.handleClick = this.handleClick.bind(this)      
     }
 
     toggle() {
@@ -65,8 +64,9 @@ class App extends Component {
         return  this.state.data.length > 1
             ?   this.state.data.map((item,index) => (  
                     <div className="react-grid-item grid-item" key={index}>
+                        {"orig " + console.log(this.gridorder)}
                         <div> {item.name} </div>
-                        <div> {item.gridorder} </div>
+                        <div> {item.gridorder} </div> 
                         <div> {item.designer} </div>
                         <img src={item.image} alt="product" />
                         <div> {item.sku} </div>
@@ -76,30 +76,56 @@ class App extends Component {
             : null
     } // END
 
+//////////////////////////////////////////////////////
+
+// TODO: I need to get new_sku:old_gridorder
+
     // RERENDER LAYOUT AND CAPTURE NEW INDEX. NEED ENTIRE OBJECT HERE
     onLayoutChange = (layout) => {
+        // Mutating a lot of data here... 
         this.newOrder = layout.map(li => ({...li, pos:li.y*3 + li.x}))  
-        console.log("neworder is ", this.newOrder)
+        console.log("layout ", this.newOrder)
+
+        // Can I grab old_gridorder here? This shows actual and new indexes
         this.newOrder = _.sortBy(this.newOrder, 'pos').map(li => parseInt(li.i))
-        console.log("this.newOrder rerendered = ", this.newOrder)
+        console.log("Rerendered. Shows actual index to old index = ", this.newOrder)
+
+         // This passes the NEW grid order separately. But here i already have this
+         this.newGridorder = this.newOrder.map(i => this.state.data[i]['gridorder'])
+         console.log("new grid ", this.newGridorder)
+
+        // this is the only thing i need from new
+         this.newsku = this.newOrder.map(i => this.state.data[i]['sku'])
+         console.log("new skus ", this.newsku)
+
+
+        //  // Somehow ive got to get orig grid OLD GRID 
+        // this.oldOrder = this.state.data.map((item) => (
+        //     {item.gridorder}
+        // ))
+
+
+//////////////////////////////////////////////////////
+
+        // here we are loading the data into it. This is what I am passing to export right now
         this.newOrder = this.newOrder.map(i => this.state.data[i])
-
         console.log("with data here ", this.newOrder)
-        console.log("and now for your viewing pleasure, a sku ", this.newOrder[0].sku)
-        this.newOrder = this.newOrder.map(i => this.state.data.sku)
-        console.log("a whole bunch of skus ", this.newOrder)
+       
+    } // END
 
-    }
+    // Maybe the easiest way to do it is NOT change the gridorder at all. Remove it! 
+    // It comes in, in ascending numberical order by definition
+    // Simply do not advance it.
+    // So that would be this.state.data.gridorder 
+    // I can even write that as static? 
+    // It is a property of the data once imported
+
 
     // ON CLICK PARSE AND SAVE CSV
     handleClick = (event) => {
-        this.newOrder = this.newOrder.map(i => this.state.data.sku[i]) 
-        console.log("from click handler", this.newOrder)       
 
-        // Verify object
-        console.log("this.newOrder from click handler = ", this.newOrder)
-        console.log("gridorder is ", this.newOrder.gridorder)
-
+        // console.log( "god only knows ", this.state.data.sku)
+   
         // TODO: BEFORE PARSE 
         // 1. Remove extra fields leaving only { config_sku:grid_order }. This is for the export
         // 2. Pull out item.gridorder into an array
@@ -110,6 +136,7 @@ class App extends Component {
         // PARSE
         const csv = Papa.unparse(this.newOrder) 
         console.log("csv from parser = ", csv)
+
         // saveAs(csv)
         var file = new File([csv], "neworder.csv", {type: "text/plain;charset=utf-8"});
         FileSaver.saveAs(file);
@@ -164,12 +191,16 @@ class App extends Component {
                     <Container>
                         <h4> Import CSV </h4>
                     <Input type="file" onChange={this.handleChange}  />
+
+                    <div className="note" > Export neworder CSV </div>
+                            <Button onClick={this.handleClick} color="secondary" size="sm">Export CSV</Button>
+
+
                     </Container>
                 </Jumbotron>
 
                 <Container className="album ">
                     <div className="note" > BUG: Drag tiles from text, not image </div>
-                    <div className="note" > BUG: Drag order will be changed </div>
 
                     <GridLayout  
                         compactType="horizontal" 
@@ -191,8 +222,9 @@ class App extends Component {
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="ml-auto" navbar>
                         <NavItem>
-                            <div className="note" > Export neworder CSV </div>
-                            <Button onClick={this.handleClick} color="secondary" size="sm">Export CSV</Button>
+                            yo
+                            {/* <div className="note" > Export neworder CSV </div>
+                            <Button onClick={this.handleClick} color="secondary" size="sm">Export CSV</Button> */}
                         </NavItem>
                        
                         </Nav>
